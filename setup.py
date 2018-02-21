acd8622abb928df7709a6f4a3db4db02d9ec57f0
Dirk Eschler <d.eschler@nmy.de>
2018-02-21 17:04:54 +0100

    Removed dependency on pypandoc.

--- setup.py
+++ setup.py
@@ -1,22 +1,24 @@
 from distutils.core import setup
+
 import djhtcpcp
-import pypandoc
+
 
 setup(
     name='django-htcpcp',
     packages=[
-      'djhtcpcp'
-     ],
+        'djhtcpcp'
+    ],
     install_requires=[
-          'setuptools',
-          'Django>=1.8'
-     ],
+        'setuptools',
+        'Django>=1.8'
+    ],
     version=djhtcpcp.__version__,
     description='Django HyperText Coffee Pot Protocol Middleware (HTCPCP)',
     author='Dash Winterson',
     author_email='dashdanw@gmail.com',
     url='https://github.com/dashdanw/django-htcpcp',
-    download_url='https://github.com/dashdanw/django-htcpcp/archive/{}.tar.gz'.format(djhtcpcp.__version__),
+    download_url='https://github.com/dashdanw/django-htcpcp/archive/{}.tar.gz'.format(
+        djhtcpcp.__version__),
     keywords=[
       'htcpcp',
       'django',
@@ -25,5 +27,4 @@ setup(
       'coffee'
     ],
     classifiers=[],
-    long_description=pypandoc.convert('README.md', 'rst')
 )
